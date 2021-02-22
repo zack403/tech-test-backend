826604b3e620ebd467e44375acf1b5d95aab214a
Zack <aminuzack7@gmail.com>
2021-02-22 12:49:19 +0100

    generated migrations

---
+++ TechTestBackend.DataAccess/Migrations/20210222114548_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechTestBackend.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "text", nullable: false),
+                    CardHolder = table.Column<string>(type: "text", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SecurityCode = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: true),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_Id",
+                table: "Payment",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_State",
+                table: "Payment",
+                column: "State");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
